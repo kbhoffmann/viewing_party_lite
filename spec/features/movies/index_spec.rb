a938870f719fe28932fca432891c32de83312a73
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-02-02 20:38:25 -0700

    add id to search_movieporo, make each movie title a link, need to add route to movie show page

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -5,9 +5,28 @@ RSpec.describe "Movie Index Page" do
 
   it 'has a button to return to the discover page', :vcr do
     visit "/users/#{user_1.id}/movies"
-    
+
     click_button "Discover Page"
 
     expect(current_path).to eq("/users/#{user_1.id}/discover")
+  end
+
+  it 'links each movie title to the movie show page', :vcr do
+    visit "/users/#{user_1.id}/movies"
+
+    #not sure how to test/mock this
+    movie = SearchMovie.new(title: "The Hangover", vote_average: 8.9, id: 27)
+
+    # within("#movie-#{movie.id}") do
+      # click_link "#{movie.title}"
+    # expect(page).to have_content(movie.vote_average)
+    expect(page).to have_link("#{movie.title}")
+
+    #had trouble with the within block, tried using sytax below.
+    # first(:link, "#{movie.title}").click
+    # end
+
+    # should be the current path when link is clicked...
+    # expect(current_path).to eq("/users/#{user_1.id}/movies/#{movie.id}")
   end
 end
