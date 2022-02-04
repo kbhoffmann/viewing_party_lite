e8c0cbb144a920293a5d94bfa8c766daa58192bd
Chris Hewitt <agop5134@gmail.com>
2022-02-04 10:35:59 -0700

    Finish a buggy project!

--- spec/features/partys/new_spec.rb
+++ spec/features/partys/new_spec.rb
@@ -3,14 +3,13 @@ RSpec.describe 'the new party page' do
   let!(:user_1) { User.create!(name: 'George Washington', email: 'george@csu.edu') }
   let!(:user_2) { User.create!(name: 'Billy Bo', email: 'bill@csu.edu') }
 
-  it 'has a form to create a new party' do
+  it 'has a form to create a new party', :vcr do
     movie_id = 33
     movie_title = 'Unforgiven'
     visit "/users/#{user_1.id}/movies/#{movie_id}/viewing-party/new?runtime=2.17"
     fill_in :duration, with: '300'
     fill_in :date, with: '2/2/2022'
     fill_in :start_time, with: '5:30'
-    # check('George Washington(george@csu.edu)', allow_label_click: true)
     within "#user-#{user_1.id}" do
       check
     end
@@ -19,6 +18,11 @@ RSpec.describe 'the new party page' do
     end
     click_button 'Create Party'
     expect(current_path).to eq("/users/#{user_1.id}")
+    within "#hosting" do
+    expect(page).to have_link("Unforgiven")
+    click_link("Unforgiven")
+    expect(current_path).to eq("/users/#{user_1.id}/movies/#{movie_id}")
+    end
   end
 
   it 'has a form to create a new party(sad path, duration too short)' do
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,28 +2,28 @@ require 'rails_helper'
 RSpec.describe 'the user show(dashboard) page' do
   let!(:user_1) { User.create!(name: 'George Washington', email: 'george@csu.edu') }
   let!(:party_1) do
-    Party.create!(movie_id: 1, date: Time.new(2022, 2, 1), duration: 120, start_time: Time.new(2022, 2, 1, 10, 30, 2))
+    Party.create!(movie_id: 33, date: Time.new(2022, 2, 1), duration: 120, start_time: Time.new(2022, 2, 1, 10, 30, 2))
   end
   let!(:party_2) do
-    Party.create!(movie_id: 1, date: Time.new(2022, 2, 1), duration: 50, start_time: Time.new(2022, 2, 1, 10, 30, 2),
+    Party.create!(movie_id: 33, date: Time.new(2022, 2, 1), duration: 50, start_time: Time.new(2022, 2, 1, 10, 30, 2),
                   host_id: user_1.id)
   end
   let!(:user_party_1) { UserParty.create!(user_id: user_1.id, party_id: party_1.id) }
   let!(:user_party_2) { UserParty.create!(user_id: user_1.id, party_id: party_2.id) }
 
-  it "displays the user's dashboard" do
+  it "displays the user's dashboard", :vcr do
     visit "/users/#{user_1.id}"
     expect(page).to have_content("George Washington's Dashboard")
   end
 
-  it 'has a button to discover movies' do
+  it 'has a button to discover movies', :vcr do
     visit "/users/#{user_1.id}"
 
     click_button('Discover Movies')
     expect(current_path).to eq("/users/#{user_1.id}/discover")
   end
 
-  it 'has all the details of the party the user is invited to' do
+  it 'has all the details of the party the user is invited to', :vcr do
     visit "/users/#{user_1.id}"
 
     within('#invited') do
@@ -33,7 +33,7 @@ RSpec.describe 'the user show(dashboard) page' do
     end
   end
 
-  it 'shows the parties where the user is a host' do
+  it 'shows the parties where the user is a host', :vcr do
     visit "/users/#{user_1.id}"
 
     within('#hosting') do
