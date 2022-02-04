e1f3cebb304dcb20da058598acb7867b3a43c735
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-02-03 17:24:33 -0700

    add tests for .all_genres, .format_reviews, .format_cast

--- spec/poros/movie_detail_spec.rb
+++ spec/poros/movie_detail_spec.rb
@@ -31,5 +31,73 @@ RSpec.describe MovieDetail do
     expect(movie.reviews).to be_a(Hash)
     expect(movie.review_count).to eq(3)
   end
-  #still need tests for all the methods
+
+  it 'formats the cast into a hash of a actor name and the actors character' do
+    movie_details = {
+                  id: 27,
+                  title: "A Movie",
+                  runtime: 115,
+                  genres: [{id: 2, name: "Comedy"}, {id: 4, name: "Action"}],
+                  overview: "Hilarous comedy about a guy",
+                  vote_average: 2.5,
+                    }
+    movie_cast = [{name: "John Doe", character: "Funny Guy"},
+                  {name: "Jane Doe", character: "Funny Gal"}]
+
+    movie_reviews = [{author: "reviewer_1", content: "This movie was terrible"},
+                     {author: "reviewer_2", content: "This movie was ok"},
+                     {author: "reviewer_3", content: "It was better while drinking"}]
+
+    movie = MovieDetail.new(movie_details, movie_cast, movie_reviews)
+
+    cast_hash = {"John Doe"=>"Funny Guy", "Jane Doe"=>"Funny Gal"}
+
+    expect(movie.format_cast(movie_cast)).to eq(cast_hash)
+  end
+
+  it 'formats the reviews into a hash of an author and the authors review' do
+    movie_details = {
+                  id: 27,
+                  title: "A Movie",
+                  runtime: 115,
+                  genres: [{id: 2, name: "Comedy"}, {id: 4, name: "Action"}],
+                  overview: "Hilarous comedy about a guy",
+                  vote_average: 2.5,
+                    }
+    movie_cast = [{name: "John Doe", character: "Funny Guy"},
+                  {name: "Jane Doe", character: "Funny Gal"}]
+
+    movie_reviews = [{author: "reviewer_1", content: "This movie was terrible"},
+                     {author: "reviewer_2", content: "This movie was ok"},
+                     {author: "reviewer_3", content: "It was better while drinking"}]
+
+    movie = MovieDetail.new(movie_details, movie_cast, movie_reviews)
+
+    reviews_hash= {"reviewer_1"=>"This movie was terrible",
+                   "reviewer_2"=>"This movie was ok",
+                   "reviewer_3"=>"It was better while drinking"}
+    expect(movie.format_reviews(movie_reviews)).to eq(reviews_hash)
+  end
+
+  it 'makes an array of all the genre names for a movie' do
+    movie_details = {
+                  id: 27,
+                  title: "A Movie",
+                  runtime: 115,
+                  genres: [{id: 2, name: "Comedy"}, {id: 4, name: "Action"}],
+                  overview: "Hilarous comedy about a guy",
+                  vote_average: 2.5,
+                    }
+    movie_cast = [{name: "John Doe", character: "Funny Guy"},
+                  {name: "Jane Doe", character: "Funny Gal"}]
+
+    movie_reviews = [{author: "reviewer_1", content: "This movie was terrible"},
+                     {author: "reviewer_2", content: "This movie was ok"},
+                     {author: "reviewer_3", content: "It was better while drinking"}]
+
+    movie = MovieDetail.new(movie_details, movie_cast, movie_reviews)
+
+    genre_array = ["Comedy", "Action"]
+    expect(movie.all_genres(movie_details)).to eq(genre_array)
+  end
 end
