37de260a49e4f5bb65ae7550370bc628dbc7cd8c
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-02-16 14:16:23 -0700

    add link to login page from welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
+  get '/login', to: 'login#index'
+
   get '/register', to: 'users#new'
 
   get '/users/:id', to: 'users#show'
